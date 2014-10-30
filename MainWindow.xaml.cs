602352c197c9ffa5b055193ba5ce32ce1fb6c173
fgaebler <frank_gaebler@yahoo.de>
2014-10-30 23:11:40 +0100

    mainwindow + menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
 namespace XMLCommander
 {
     /// <summary>
@@ -20,9 +23,55 @@ namespace XMLCommander
     /// </summary>
     public partial class MainWindow : Window
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        //Attach this to the MouseDown event of your drag control to move the window in place of the title bar
+        private void WindowDrag(object sender, MouseButtonEventArgs e) // MouseDown
+        {
+            ReleaseCapture();
+            SendMessage(new WindowInteropHelper(this).Handle,
+                0xA1, (IntPtr)0x2, (IntPtr)0);
+        }
+
+        //Attach this to the PreviewMousLeftButtonDown event of the grip control in the lower right corner of the form to resize the window
+        private void WindowResize(object sender, MouseButtonEventArgs e) //PreviewMousLeftButtonDown
+        {
+            HwndSource hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
+            SendMessage(hwndSource.Handle, 0x112, (IntPtr)61448, IntPtr.Zero);
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Save_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_SaveAs_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Close_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
